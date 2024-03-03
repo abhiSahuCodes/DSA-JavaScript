a9d0192ea9d9f64ac6e91cbaf34bf3dc7ed38040
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-03-03 15:07:06 +0530

    added comment on each loop operation

--- 01_basic_array/01_addElement-array.js
+++ 01_basic_array/01_addElement-array.js
@@ -4,11 +4,38 @@ let position = 6;
 
 let number = 24;
 
-for (let i = arr.length -1; i >= position; i--) {
-    arr[i + 1] = arr[i];
-    if (i == position) {
-        arr[i] = number;
-    }
+for (let i = arr.length - 1; i >= position; i--) {
+  arr[i + 1] = arr[i];
+  if (i == position) {
+    arr[i] = number;
+  }
 }
 
 console.log(arr);
+
+// first loop
+/*
+i = 9;
+arr[10] = arr[9];  [5, 7, 9, 10, 10]
+
+*/
+// second loop
+/*
+i = 8;
+arr[9] = arr[8];  [5, 7, 9, 9, 10]
+
+*/
+// third loop
+/*
+i = 7;
+arr[8] = arr[7];  [5, 7, 7, 9, 10]
+
+*/
+// fourth loop
+/*
+i = 6;
+arr[7] = arr[6];  [5, 5, 7, 9, 10]
+arr[6] = 24
+
+// Hence, arr = [25, 66, 32, 43, 35, 12, 24, 5, 7, 9, 10]
+*/
