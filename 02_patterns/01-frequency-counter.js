d1aaedfcf3c106293264aa965e2536e440d9ba27
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-03-26 22:28:27 +0530

    refactored version reducing time complexity

--- 02_patterns/01-frequency-counter.js
+++ 02_patterns/01-frequency-counter.js
@@ -26,4 +26,41 @@ function same(array1, array2) {
 
 const array1 = [1, 2, 3, 2];
 const array2 = [9, 1, 4, 4];
-console.log(same(array1, array2));
+// console.log(same(array1, array2));
+
+// Refactored 
+
+function sameRefact(arr1, arr2) {
+    if (arr1.length !== arr2.length) return;
+
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+
+    for (let key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false;
+        }
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const arr1 = [1, 5, 3, 3];
+const arr2 = [9, 1, 25, 9];
+console.log(sameRefact(arr1, arr2));
+
+
+
+
+
+
