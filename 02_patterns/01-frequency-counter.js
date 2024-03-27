13e024a9e066e4eb713c86cee22bd88d1e6c7c65
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-03-27 21:52:58 +0530

    anagram freq counter

--- 02_patterns/01-frequency-counter.js
+++ 02_patterns/01-frequency-counter.js
@@ -7,60 +7,94 @@ same.
 
 // This is O(n2) as it is looping over one array and other in nested.
 function same(array1, array2) {
-    if (array1.length !== array2.length) {
-        return false;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    //Finding the index of the square of the number in array2
+    let corrIndex = array2.indexOf(array1[i] ** 2);
+    //If not present
+    if (corrIndex === -1) {
+      return false;
     }
-    for (let i = 0; i < array1.length; i++) {
-        //Finding the index of the square of the number in array2
-        let corrIndex = array2.indexOf(array1[i] ** 2);
-        //If not present
-        if (corrIndex === -1) {
-            return false;
-        }
-        //If present then removing it from array2
-        array2.splice(corrIndex, 1);
-    }
-    //When all are removed after the loop is over
-    return true;
+    //If present then removing it from array2
+    array2.splice(corrIndex, 1);
+  }
+  //When all are removed after the loop is over
+  return true;
 }
 
 const array1 = [1, 2, 3, 2];
 const array2 = [9, 1, 4, 4];
 // console.log(same(array1, array2));
 
-// Refactored 
+// Refactored
 
 function sameRefact(arr1, arr2) {
-    if (arr1.length !== arr2.length) return;
+  if (arr1.length !== arr2.length) return;
 
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
 
-    for (let val of arr1) {
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const arr1 = [1, 5, 3, 3];
+const arr2 = [9, 1, 25, 9];
+// console.log(sameRefact(arr1, arr2));
+
+// Anagram
+/*
+Given two strings, write a function to determine if the second
+string is an anagram of the first. An anagram is a word, phrase,
+or name formed by rearranging the letters of another, such as 'cinema',
+formed from 'iceman'.
+*/
+
+function anagramAnalyzer(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
     }
 
-    for (let val of arr2) {
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    let freqCounter1 = {};
+    let freqCounter2 = {};
+
+    for (let i = 0; i < str1.length; i++) {
+        freqCounter1[str1[i]] = (freqCounter1[str1[i]] || 0) + 1;
     }
 
-    for (let key in frequencyCounter1) {
-        if (!(key ** 2 in frequencyCounter2)) {
+    for (let i = 0; i < str2.length; i++) {
+        freqCounter2[str2[i]] = (freqCounter2[str2[i]] || 0) + 1;
+    }
+
+    for (let key in freqCounter1) {
+        if (!(key in freqCounter2)) {
             return false;
         }
-        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+        if (freqCounter1[key] !== freqCounter2[key]) {
             return false;
         }
     }
     return true;
 }
 
-const arr1 = [1, 5, 3, 3];
-const arr2 = [9, 1, 25, 9];
-console.log(sameRefact(arr1, arr2));
+const string1 = 'texttwisttime';
+const string2 = 'timetwisttext';
 
-
-
-
-
-
+console.log(anagramAnalyzer(string1, string2));
