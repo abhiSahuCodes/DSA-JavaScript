7c08ede5d64f1e3f4b2e003695ed0cc998704cea
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-03-31 22:13:29 +0530

    empty array fix in method one of count unique values

--- 02_patterns/02-multiple-pointers.js
+++ 02_patterns/02-multiple-pointers.js
@@ -36,6 +36,7 @@ const array = [-4, -3, -2, -1, 0, 1, 2, 5];
 
 // count unique values
 function countingUniqueValues(arr) {
+  if (arr.length === 0) return 0;
   let i = 0;
   for (let j = 1; j < arr.length; j++) {
     if (arr[i] !== arr[j]) {
