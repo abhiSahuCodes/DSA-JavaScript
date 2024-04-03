f254087b1e8150ed7c7e76fe4acd605c0bd8d700
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-04-03 23:11:53 +0530

    sliding window method: sub array max sum O(n)

--- 02_patterns/03-sliding-window-pattern.js
+++ 02_patterns/03-sliding-window-pattern.js
@@ -14,7 +14,6 @@ function maxSubarraySum(arr, sum) {
         if (tempSum > maxSum) {
             maxSum = tempSum;
         }
-        console.log(tempSum, maxSum);
     }
     return maxSum;
 }
@@ -23,4 +22,25 @@ function maxSubarraySum(arr, sum) {
 const array = [2, 6, 9, 2, 1, 8, 5, 6, 3];
 const num = 3;
 
-console.log(maxSubarraySum(array, num));
+// console.log(maxSubarraySum(array, num));
+
+// optimal solution sliding window method
+
+function subarrayMaxSum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (num > arr.length) {
+        return null;
+    }
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i-num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(subarrayMaxSum(array, num));
