ce33de367745934fa756db0a4a12a25c332b2abf
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-04-13 20:34:12 +0530

    searching an element in a sorted array using O(log n) time complexity method

--- 02_patterns/04-divide-conquer-pattern.js
+++ 02_patterns/04-divide-conquer-pattern.js
@@ -15,6 +15,33 @@ function searchValue(array, val) {
 
 const arr = [3, 4, 6, 8, 11, 14, 17, 18, 30, 45, 66, 71, 75, 92, 103, 106, 234, 235, 345, 543, 565];
 
-let val = 234;
+let val = 103;
 
 console.log(searchValue(arr, val));
+
+// Time Complexity: O(log n)
+
+function searchEfficient(array, val) {
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middleIndex = Math.floor((max + min) / 2);
+        let currentElement = array[middleIndex];
+
+        if (currentElement < val) {
+            min = middleIndex + 1;
+        }
+
+        else if (currentElement > val) {
+            max = middleIndex -1;
+        }
+
+        else {
+            return middleIndex;
+        }
+    }
+    return -1;
+}
+
+console.log(searchEfficient(arr, val));
