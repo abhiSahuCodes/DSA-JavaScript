0e6ccc143bb19bccd2eee165fe5ccc4537e6800b
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-04-16 23:28:18 +0530

    average pair solution with TC = O(n) and SC = O(1)

--- 03_patterns_questions/2-patterns-questions.js
+++ 03_patterns_questions/2-patterns-questions.js
@@ -14,3 +14,22 @@ function averagePair(arr, target) {
 
 console.log(averagePair([1, 2, 3, 5, 8, 10, 11], 9));
 
+// Solution with TC = O(n) and SC = O(1)
+function averagePairNums(arr, target) {
+    let left = 0;
+    let right = arr.length -1;
+
+    while(left < right) {
+        if ((arr[left] + arr[right]) / 2 === target) {
+            return true;
+        }
+        else if ((arr[left] + arr[right]) / 2 > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return false;
+}
+
+console.log(averagePairNums([1, 2, 3, 5, 8, 10, 11], 5));
